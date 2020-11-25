c6cb379c2b58fc0110e2f2d05b2a1d7faa3d0976
Jessica Zanelato Soares <jessica.soares@portobello.com.br>
2020-11-25 19:49:31 -0300

    Melhorias arquivos websocket

--- semana5/websocket/chat.js
+++ semana5/websocket/chat.js
@@ -1,4 +1,4 @@
-var ws = new WebSocket("ws://localhost:8000");
+var ws = new WebSocket("ws://localhost:5000");
 ws.onopen = function(e) {
     console.log("Conexão estabelecida", e);
 }
--- semana5/websocket/wsserver.js
+++ semana5/websocket/wsserver.js
@@ -1,6 +1,9 @@
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({host: 'localhost', port: 8000 });
+const port = 5000
+const host = 'localhost'
+
+const wss = new WebSocket.Server({host: host, port: port });
 
 wss.on('connection', ws => {
 	ws.on('message', mensagem => {
@@ -10,4 +13,4 @@ wss.on('connection', ws => {
 	});
 	ws.send('Estou te ouvindo!');
 });
-console.log('Servidor rodando...');
+console.log('Servidor rodando... //'+ host +':' + port);
